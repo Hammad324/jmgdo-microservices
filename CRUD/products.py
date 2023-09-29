c80762e8fe355815e462cfe5efbdac3d5a180e32
Hammad-Farrukh <hammadfarrukh602@gmail.com>
2023-09-29 22:17:05 +0500

    mm

--- CRUD/products.py
+++ CRUD/products.py
@@ -11,8 +11,58 @@ products = [
     {'id': 144, 'name': 'Black Marker', 'price': 1.99}
 ]
 
-#
-#Add all the REST API end-points here
-#
+# Example request - http://localhost:5000/products
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+# Example request - http://localhost:5000/products/144 - with method GET
+@app.route('/products/<id>', methods=['GET'])
+def get_product(id):
+    id = int(id)
+    product = [x for x in products if x["id"] == id][0]
+    return jsonify(product)
+
+# Example request - http://localhost:5000/products - with method POST
+@app.route('/products', methods=['POST'])
+def add_product():
+    products.append(request.get_json())
+    return '', 201
+
+# Example request - http://localhost:5000/products/144 - with method PUT
+@app.route('/products/<id>', methods=['PUT'])
+def update_product(id):
+    id = int(id)
+    updated_product = json.loads(request.data)
+    product = [x for x in products if x["id"] == id][0]
+    for key, value in updated_product.items():
+        product[key] = value
+    return '', 204
+
+# Example request - http://localhost:5000/products/144 - with method DELETE
+@app.route('/products/<id>', methods=['DELETE'])
+def remove_product(id):
+    id = int(id)
+    product = [x for x in products if x["id"] == id][0]
+    products.remove(product)
+    return '', 204
 
 app.run(port=5000,debug=True)
+
+
+# cd jmgdo-microservices/CRUD
+# for using cURL command run the server and in another terminal write curl http://localhost:5000/products
+"""Add by curl -X POST -H "Content-Type: application/json" \
+    -d '{"id": 145, "name": "Pen", "price": 2.5}' \
+    http://localhost:5000/products"""
+# use postman for practice
+"""{
+    "id":146,
+    "name":"Laptop Bag",
+    "price":45.00
+} Edit price with postman"""
+"""Add this product 
+{"id":142,
+    "name":"Eraser",
+    "price":1.50}"""
+# Delete the product with id 142.
